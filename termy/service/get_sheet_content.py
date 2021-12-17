455f03b0325d2dc6d56d9a9cf7785fb2c4c360ec
bhanumusuluri <bhanumusuluri@gmail.com>
2021-12-17 19:47:43 +0530

    made changes for API-KEY support

--- termy/service/get_sheet_content.py
+++ termy/service/get_sheet_content.py
@@ -1,8 +1,12 @@
+import json
 import os.path
+import csv
 
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 import csv
+import requests
+import pandas as pd
 
 from termy.constants import TERMY_COMMANDS_FILE
 
@@ -30,3 +34,19 @@ def get_sheet_content_into_csv(SAMPLE_SPREADSHEET_ID , SAMPLE_RANGE_NAME, creds)
         file_with_commands.close()
     except HttpError as err:
         print(err)
+
+
+def get_sheet_content_to_csv(SAMPLE_SPREADSHEET_ID , SAMPLE_RANGE_NAME, API_KEY):
+    url = "https://sheets.googleapis.com/v4/spreadsheets/"+SAMPLE_SPREADSHEET_ID +"/values/"+SAMPLE_RANGE_NAME
+    params = {"key": API_KEY}
+    headers = {"Content-Type": "application/json"}
+    response = requests.get(url, params=params, headers=headers)
+    final_dictionary = json.loads(response.text)
+    if (os.path.exists(TERMY_COMMANDS_FILE) and os.path.isfile(TERMY_COMMANDS_FILE)):
+        os.remove(TERMY_COMMANDS_FILE)
+    file_with_commands = open(TERMY_COMMANDS_FILE, 'w')
+    csv_writer = csv.writer(file_with_commands)
+
+    for emp in final_dictionary.get("values"):
+        csv_writer.writerow(emp)
+    file_with_commands.close()
