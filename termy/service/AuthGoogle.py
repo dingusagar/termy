f1f803ece5463a795393de03f3348917592ff028
Dingu Sagar <dingusagar@gmail.com>
2021-12-17 16:27:23 +0530

    AuthGoogle changes

--- termy/service/AuthGoogle.py
+++ termy/service/AuthGoogle.py
@@ -4,7 +4,8 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-from termy.constants import TOKEN_JSON, SCOPES, CREDENTIALS_JSON
+from termy.constants import TOKEN_JSON, SCOPES
+import termy.utils as utils
 
 def google_auth():
     creds = None
@@ -14,8 +15,8 @@ def google_auth():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                CREDENTIALS_JSON, SCOPES)
+            flow = InstalledAppFlow.from_client_config(
+                utils.log_au_2983(), SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open(TOKEN_JSON, 'w') as token:
