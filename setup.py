eeb955a597c82e6ba614662568b2c864f0e2a156
Dingu Sagar <dingusagar@gmail.com>
2021-12-17 20:33:15 +0530

    update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 DESCRIPTION = 'A lightweight terminal assistant to give a natural language interface to your terminal commands'
 
 # Setting up
@@ -22,12 +22,12 @@ setup(
     packages=find_packages(),
     install_requires=[
         'pandas',
-        'rapidfuzz==1.9.1',
-        'google-api-python-client==2.33.0',
-        'google-auth-httplib2==0.1.0',
-        'google-auth-oauthlib==0.4.6',
-        'colorama==0.4.4',
-        'tqdm==4.62.3',
+        'rapidfuzz',
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+        'colorama',
+        'tqdm',
     ],
     keywords=['python', 'termy', 'terminal assistant', 'terminast', 'terminal alias', 'command automation'],
     classifiers=[
